3bbe3afc39a9dbcc1d7a9a290fe10777e596c620
Pranav Rajan <rajan.pranav320@gmail.com>
2020-04-30 20:31:31 -0700

    solved isPalindrome

---
+++ is_palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        original = str(x)
+        if original == original[::-1]:
+            return True
+        return False
+        
+        
