d1f77d0d33e42ee1e45897de76d36374ecb0be70
Pranav Rajan <rajan.pranav320@gmail.com>
2020-04-30 19:33:25 -0700

    cleaned up stuff

--- two_sum.py
+++ two_sum.py
@@ -1,15 +1,8 @@
-nums = [2, 7, 11, 15]
-target = 9
-
-def twoSum(nums, target_value):
-    # map that maps value to its index
-    value_map = dict()
-    for i, value in enumerate(nums):
-        # find the difference of the target and its values
-        difference = target - value
-        if difference in value_map:
-            return [value_map[difference], i]
-        value_map[value] = i
-
-foo = twoSum(nums, target)
-print(foo)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        value_map = dict()
+        for i, value in enumerate(nums):
+            difference = target - value
+            if difference in value_map:
+                return [value_map[difference], i]
+            value_map[value] = i
