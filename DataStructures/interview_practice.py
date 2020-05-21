e9d11a2d0a9852c4b1e2591345a8dc67840c2c15
Pranav Rajan <rajan.pranav320@gmail.com>
2020-05-21 11:04:49 -0700

    finished single number

--- DataStructures/interview_practice.py
+++ DataStructures/interview_practice.py
@@ -11,20 +11,45 @@
 #
 # print(buySellStock(input))
 
-input = [1, 2, 3, 4, 5, 6, 7]
-k = 3
+# input = [1, 2, 3, 4, 5, 6, 7]
+# k = 3
+#
+#
+#
+# def reverse(arr, start, end):
+#     while start < end:
+#         arr[start], arr[end] = arr[end], arr[start]
+#         start, end = start + 1, end - 1
+# def rotate(arr, k):
+#     n = len(arr)
+#     k %= n
+#     reverse(arr, 0, n - 1)
+#     reverse(arr, 0, k - 1)
+#     reverse(arr, k, n - 1)
+#
+# rotate(input, 3)
+
+input = [4, 1, 2, 1, 2]
+def singleNumber(arr):
+    arr.sort()
+    i = 0
+    while i in range(len(arr)):
+        value = arr[i]
+        sub_arr = arr[i + 1:]
+        if value in sub_arr:
+            i = sub_arr.index(value)
+            arr = sub_arr[i + 1:]
+            print(i)
+        else:
+            return arr[i]
+    # for i in range(len(arr)):
+    #     value = arr[i]
+    #     sub_arr = arr[i + 1:]
+    #     if value in sub_arr:
+    #         i = sub_arr.index(value) + 1
+    #         continue
+    #     else:
+    #         return arr[i]
 
 
-
-def reverse(arr, start, end):
-    while start < end:
-        arr[start], arr[end] = arr[end], arr[start]
-        start, end = start + 1, end - 1
-def rotate(arr, k):
-    n = len(arr)
-    k %= n
-    reverse(arr, 0, n - 1)
-    reverse(arr, 0, k - 1)
-    reverse(arr, k, n - 1)
-
-rotate(input, 3)
+singleNumber(input)
