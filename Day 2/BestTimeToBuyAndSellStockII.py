b0564be9c42b6884cbdcd1947c1113d24e087aa9
Pranav Rajan <rajan.pranav320@gmail.com>
2020-05-26 14:12:13 -0700

    finished buy and sell stock II

---
+++ Day 2/BestTimeToBuyAndSellStockII.py
@@ -0,0 +1,12 @@
+# Runtime Complexity: O(n) because of the for loop
+# Space Complexity: O(n) for all the elements in the list 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+        
+        return max_profit
+        
+        
