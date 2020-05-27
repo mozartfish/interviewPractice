a986b3c8dd8a3e78b87f73ee2123f8952500e05c
Pranav Rajan <rajan.pranav320@gmail.com>
2020-05-27 10:33:40 -0700

    finished power of three

---
+++ Day 3/PowerOfThree.py
@@ -0,0 +1,12 @@
+# Runtime complexity: O(log3(n))
+# Space complexity: O(1) since we do not use additional memory
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n < 1:
+            return False
+        
+        while n % 3 == 0:
+            n /= 3
+        
+        return n == 1
+        
